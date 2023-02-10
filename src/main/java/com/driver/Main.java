21ec0f2e7838f580d2b53f3e581d19b8197d00cd
unknown <vipulsingh.cg@gmail.com>
2023-02-11 00:22:31 +0530

    submit solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String [] arg){
+      RWOnly obj = new RWOnly();
+      obj.setName("vipul");
+      obj.getName();
+  }
 }
